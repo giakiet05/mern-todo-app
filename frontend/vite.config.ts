16675556602282e087eff9f0642e728e83fe1c9b
giakiet05 <giakiet05@gmail.com>
2024-12-07 14:28:34 +0700

    lại sửa

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,19 +1,14 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig(({ mode }) => {
-	// Load env variables based on `mode` (e.g., 'development' or 'production')
-	const env = loadEnv(mode, process.cwd(), '');
-
-	return {
-		plugins: [react()],
-		server: {
-			proxy: {
-				'/api': {
-					target: 'https://mern-todo-app-backend-ta4u.onrender.com' // Use env variable with a fallback
-				}
+export default defineConfig({
+	plugins: [react()],
+	server: {
+		proxy: {
+			'/api': {
+				target: 'https://mern-todo-app-backend-ta4u.onrender.com'
 			}
 		}
-	};
+	}
 });
