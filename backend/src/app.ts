f4045d6e86d0554e19d53753091b0edcebe89652
giakiet05 <giakiet05@gmail.com>
2024-12-07 21:08:30 +0700

    lại sửa cookie

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -13,18 +13,34 @@ import cors from 'cors';
 
 const app = express(); // khai báo app
 console.log(process.env.NODE_ENV || 'Không có cái này nha');
+
+// app.use(
+// 	session({
+// 		secret: env.SESSION_SECRET,
+// 		resave: false,
+// 		saveUninitialized: false,
+// 		rolling: true, // Đặt lại thời gian hết hạn session mỗi khi có hoạt động
+// 		cookie: {
+// 			maxAge: 60 * 60 * 1000, // 1 giờ
+// 			secure: false, // Không bắt buộc HTTPS
+// 			sameSite: 'lax', // Linh hoạt, hỗ trợ cả frontend và backend
+// 			httpOnly: true // Cookie chỉ được backend truy cập
+// 		},
+// 		store: MongoStore.create({
+// 			mongoUrl: env.MONGODB_CONNECTION_STRING
+// 		})
+// 	})
+// );
+
 app.use(
 	session({
 		secret: env.SESSION_SECRET,
 		resave: false,
 		saveUninitialized: false,
-		rolling: true, // Đặt lại thời gian hết hạn session mỗi khi có hoạt động
 		cookie: {
-			maxAge: 60 * 60 * 1000, // 1 giờ
-			secure: false, // Không bắt buộc HTTPS
-			sameSite: 'lax', // Linh hoạt, hỗ trợ cả frontend và backend
-			httpOnly: true // Cookie chỉ được backend truy cập
+			maxAge: 60 * 60 * 1000
 		},
+		rolling: true,
 		store: MongoStore.create({
 			mongoUrl: env.MONGODB_CONNECTION_STRING
 		})
@@ -43,20 +59,6 @@ app.use(morgan('dev'));
 app.use(express.json());
 
 //Session (use for login)
-// app.use(
-// 	session({
-// 		secret: env.SESSION_SECRET,
-// 		resave: false,
-// 		saveUninitialized: false,
-// 		cookie: {
-// 			maxAge: 60 * 60 * 1000
-// 		},
-// 		rolling: true,
-// 		store: MongoStore.create({
-// 			mongoUrl: env.MONGODB_CONNECTION_STRING
-// 		})
-// 	})
-// );
 
 app.get('/', (req, res) => {
 	res.json({
